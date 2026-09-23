4469add30eb77ce476a48b5fc34febb48344e42c
abhi-0981 <absingh0981@gmail.com>
2026-09-23 14:15:06 +0530

    update

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ app = Flask(__name__)
 RIGHT_LOGO_FILE = "logo.png"
 
 RIGHT_COVER_RECT = fitz.Rect(235, 10, 300, 45)
-RIGHT_LOGO_RECT = fitz.Rect(175, 10, 340, 48)
-
+RIGHT_LOGO_RECT = fitz.Rect(115, 8, 325, 48)
 # =========================================================
 # OPTIONAL LEFT LOGO
 # =========================================================
